6a9b052aea5c3d224a98ea1e93c13e634ad42ef6
ShuvoRoy-coder <sroy90677@gmail.com>
2025-05-15 20:17:06 +0600

    updated

--- assets/js/translations.js
+++ assets/js/translations.js
@@ -4,13 +4,25 @@ const translations = {
       aboutUs: "About us",
       contactUs: "Contact us",
       english: "English",
-      french: "French"
+      french: "French",
+      qui: "Who are we?",
+      everest: "is a holding company. It is an innovative player in the investment field, with a particular focus on three key sectors",
+      sport: "Sport",
+      evenementiel: "Event management",
+      formation: "Formation",
+      leGroupe: "The EVEREST group belongs to 3 entrepreneurs from Bordeaux.",
     },
     fr: {
       home: "Accueil",
       aboutUs: "À propos",
       contactUs: "Contactez-nous",
       english: "Anglais",
-      french: "Français"
+      french: "Français",
+      qui: "Qui sommes-nous ?",
+      everest: "est une holding de groupe. C’est un acteur innovant dans le domaine de l’investissement, avec un focus particulier sur trois secteurs clés",
+      sport: "Sport",
+      evenementiel: "Evénementiel",
+      formation: "Formation",
+      leGroupe: "Le groupe EVEREST appartient à 3 entrepreneurs bordelais",
     }
   };
